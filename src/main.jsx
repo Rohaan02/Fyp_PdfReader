07c1898cc6dd53eb111f9a7e114b65c92b2bde24
Rohaan02 <rohaannadeem2@gmail.com>
2024-12-04 16:11:38 +0500

    removed the comments

--- src/main.jsx
+++ src/main.jsx
@@ -21,9 +21,9 @@ const clientId =
   "356715745812-or5ri7vshpqsim8st9be6n3im97gv9he.apps.googleusercontent.com"; // Replace with your actual Google Client ID
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <GoogleOAuthProvider clientId={clientId}>
-    <App />
-  </GoogleOAuthProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
